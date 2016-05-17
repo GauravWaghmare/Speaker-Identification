5395b127a3e27a02021f821939cd1bd4f993f369
GauravWaghmare <gauravwaghmare30@gmail.com>
2016-05-17 17:21:25 +0530

    Update Cepstrum.py

--- Cepstrum.py
+++ Cepstrum.py
@@ -11,18 +11,33 @@ n_samples = int(fs*0.03)
 data = data[:n_samples]
 time_axis = np.array(range(n_samples))/fs
 
-data_cep = fftpack.rfft(data)
-data_cep = np.absolute(data_cep)
-data_cep = np.log10(data_cep)
+data_fft = fftpack.rfft(data)
+data_fft = np.absolute(data_fft)
+data_cep = np.log10(data_fft)
 data_cep = fftpack.irfft(data_cep)
 
-#Plot signal vs time
-first = plt.subplot(211)
+#Plot signal, cepstrum and spectrum vs time
+plt.figure(1)
+first = plt.subplot(311)
 first.plot(time_axis, data)
 first.set_title("Signal")
-second = plt.subplot(212)
+second = plt.subplot(312)
 second.plot(time_axis, data_cep)
 second.set_title("Cepstrum")
-plt.show() 
+third = plt.subplot(313)
+freqs = fftpack.rfftfreq(data.size, time_axis[1]-time_axis[0])
+third.plot(freqs, 20*np.log10(data_fft))
+third.set_title("Log magnitude spectrum") 
 
+#Plot autocorrelation
+#out1 = np.correlate(data, data, mode='same')
+#out2 = np.correlate(data, data, mode='full')
+#plt.figure(2)
+#outf = plt.subplot(211)
+#outf.set_title("Same")
+#outf.plot(out1)
+#outs = plt.subplot(212)
+#outs.plot(out2)
+#outs.set_title("Full")
 
+plt.show()
