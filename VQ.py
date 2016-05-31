f3bf43c2542a1328a9888ac4441908a9739d58f0
ManviG <manvigarg1995@gmail.com>
2016-05-31 17:37:19 +0530

    VQ on five utterances of one speaker

---
+++ VQ.py
@@ -0,0 +1,77 @@
+from sklearn.lda import LDA
+import featureExtraction
+import glob
+import numpy
+import scipy.io.wavfile
+from sklearn.preprocessing import OneHotEncoder
+import scipy.stats as stats
+from features import mfcc
+from sklearn import cluster
+
+
+frame_size = 25.0/1000.0 # to convert seconds to miliseconds
+frame_shift = 10.0/1000.0  # to convert seconds to miliseconds
+
+
+direc = "/home/manvi/Desktop/voicebiometric/Phoneme/trainset/"
+
+
+num_speakers = 5
+
+srno = 0
+
+speaker = numpy.zeros(shape=(num_speakers*4,34))
+
+
+y = []
+
+fno =0
+row = 0
+
+speakers = []
+
+
+while srno < num_speakers:
+	srno = srno+1
+	print "\nsrno = " + str(srno)
+	directory = direc + str(srno) + "/"
+	utterances = glob.glob(directory + "*.wav")
+	# feature_vector = numpy.zeros(shape=(34,1))
+	flag = False
+	# flag_2 = False
+
+	for fname in utterances:
+		fn = fname.split('/')
+		fn = fn[-1]
+		# print fn 
+		# print "fno = " + str(fno)
+		fno = fno + 1
+		row = row+1
+		y.append(srno-1)
+		fs, signal = scipy.io.wavfile.read(fname)
+		window_len = frame_size*fs # Number of samples in frame_size
+		sample_shift = frame_shift*fs # Number of samples shifted
+		features = featureExtraction.stFeatureExtraction(signal, fs, window_len, sample_shift )
+		# print "features.shape = " + str(features.shape)
+		i = 0
+
+		if flag == False:
+			flag = True
+			feature_vector = features
+			# print feature_vector.shape
+			# print
+			# i = 1
+		else:
+			# print "arbitajvhfibvvdbli"
+			feature_vector = numpy.concatenate((feature_vector, features ), axis = 1)
+			# print feature_vector.shape
+
+	# print "feature_vector = " + str(feature_vector.shape)
+	# print "speakers = " + str(speakers.shape)
+	# print
+	# speakers = numpy.append(speakers, feature_vector)
+
+
+
+print speakers.shape
+# print y
