ed8dd9f057413b33a7b5ba0c5097ca320625bc64
GauravWaghmare <gauravwaghmare30@gmail.com>
2016-06-06 12:51:05 +0530

    Made a test set

--- lstm_speaker.py
+++ lstm_speaker.py
@@ -1,18 +1,17 @@
 import numpy as np
 import numpy
 import featureExtraction
-import glob
+import glob, os
 import scipy.io.wavfile
 import scipy.cluster.vq as vq
 from sklearn.preprocessing import OneHotEncoder
 from keras.models import Sequential
 from keras.layers import LSTM, Dense
-from keras.utils.visualize_util import plot
 
 frame_size = 25.0/1000.0 # to convert seconds to miliseconds
 frame_shift = 10.0/1000.0  # to convert seconds to miliseconds
 
-direc = "/home/Gaurav/Documents/Phoneme/trainset/"
+direc = "/home/gaurav/Documents/Phoneme/trainset/"
 
 num_speakers = 5
 srno = 0
@@ -23,8 +22,11 @@ x_train = []
 while srno < num_speakers:
 	srno = srno+1
 	directory = direc + str(srno) + "/"
+	# print directory
 	utterances = glob.glob(directory + "*.wav") #returns a list of names matching the argument in the directory
+	# print utterances
 	for fname in utterances:
+		# print fname
 		fs, signal = scipy.io.wavfile.read(fname)
 		window_len = frame_size*fs # Number of samples in frame_size
 		sample_shift = frame_shift*fs # Number of samples shifted
@@ -35,13 +37,31 @@ while srno < num_speakers:
 		feature_codebook = vq.kmeans2(features,8)[0]
 		x_train.append(feature_codebook)
 
-x_train = np.array(feature_vector)
-
-y_train = np.array([0]*5+[1]*5+[2]*5+[3]*5+[4]*5)
-y_train = np.transpose(y_train)
+x_train = np.array(x_train)
+# print x_train.shape
+y_train = np.array([0]*4+[1]*4+[2]*4+[3]*4+[4]*4)
+y_train = np.reshape(y_train, (20,1))
 enc = OneHotEncoder()
-y_train = np.array(enc.fit_transform(y_train)).toarray()
+y_train = enc.fit_transform(y_train).toarray()
+# print y_train
+# print y_train.shape
 
+x_test = []
+
+directory = direc + "test/"
+utterances = glob.glob(directory + ".wav")
+for fname in utterances:
+	fs, signal = scipy.io.wavfile.read(fname)
+	window_len = frame_size*fs # Number of samples in frame_size
+	sample_shift = frame_shift*fs # Number of samples shifted
+	features = featureExtraction.stFeatureExtraction(signal, fs, window_len, sample_shift)
+	# Transposing features to whiten them
+	features = np.reshape(features, (features.shape[1], features.shape[0]))
+	features = vq.whiten(features)
+	feature_codebook = vq.kmeans2(features,8)[0]
+	x_test.append(feature_codebook)
+
+x_test = np.array(x_test)
 
 data_dim = 34 # Gaurav : Number of features 
 timesteps = 8 # Gaurav : Number of states 
@@ -68,6 +88,6 @@ model.compile(loss='categorical_crossentropy',
 # y_val = np.random.random((10, nb_classes)) # Gaurav : Sample x user
 
 model.fit(x_train, y_train, nb_epoch=5,
-          validation_data=(x_val, y_val))
+          validation_split=0.2)
 
-plot(model, to_file='model.png')
+model.predict(x_test)
