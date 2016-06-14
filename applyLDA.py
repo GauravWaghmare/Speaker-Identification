23c13cd8ee32586ba0603a5d0b4a129e975f035b
GauravWaghmare <gauravwaghmare30@gmail.com>
2016-06-14 12:37:07 +0530

    Corrections

--- applyLDA.py
+++ applyLDA.py
@@ -2,8 +2,10 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 def find_one(iterable):
 	for element in iterable:
-		if element==1:
-			return i 
+		i = 0
+		while element!=1:
+			i += 1
+		return i 
 
 def applyLDA(X, Y):
 	y = map(find_one, Y)
