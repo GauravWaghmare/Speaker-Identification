d7fa6783a9ee60701d690abb48eced8f193cd0d6
ManviG <manvigarg1995@gmail.com>
2016-06-22 09:54:05 +0530

    Class made in VQ.py

--- FinalCode/VQ.py
+++ FinalCode/VQ.py
@@ -7,86 +7,39 @@ from sklearn.preprocessing import OneHotEncoder
 import scipy.stats as stats
 from features import mfcc
 from sklearn import cluster
-import Removesilence as rs
-# from feature import mix_feature
 from keras.regularizers import l2, activity_l2
 import LPC
+import Removesilence as rs
 
 
-frame_size = 32.0/1000.0 # to convert seconds to miliseconds
-frame_shift = 16.0/1000.0  # to convert seconds to miliseconds
+class Features(object):
+	"""docstring for Features"""
+	
+	def __init__(self, frame_size, frame_shift, direc, num_speakers):
+		self.frame_size = float(frame_size)
+		self.frame_shift = float(frame_shift)
+		self.direc = direc
+		self.num_speakers = int(num_speakers)
+	
+	def getTrainingMatrix(self):
+		srno = 0
+		flag = False
+		fno =0
+		while srno < self.num_speakers:
+			srno = srno+1
+			print "\nsrno = " + str(srno)
+			directory = self.direc + str(srno) + "/"
+			utterances = glob.glob(directory + "*.wav")
 
-
-
-direc = "/home/manvi/Desktop/voicebiometric/Phoneme/trainset/"
-
-
-def getFeatureMatrix(direc, num_speakers):
-	# num_speakers = 5
-	srno = 0
-	flag = False
-
-	fno =0
-	row = 0
-	flag = False
-
-	while srno < num_speakers:
-		srno = srno+1
-		print "\nsrno = " + str(srno)
-		directory = direc + str(srno) + "/"
-		utterances = glob.glob(directory + "*.wav")
-
-		for fname in utterances:
-			fn = fname.split('/')
-			fn = fn[-1]
-			fno = fno + 1
-			row = row+1
-			print
-			print fn
-
-			try:
-				fs, signal = scipy.io.wavfile.read(fname)
-
-				print fs
-				window_len = frame_size*fs # Number of samples in frame_size
-				sample_shift = frame_shift*fs # Number of samples shifted
-
-				# print window_len
-				# print sample_shift
-				# print
-				# print signal.shape
-				# print signal.shape[0]
-
-				try:
-					if signal.shape[1]:
-						signal = numpy.mean(signal, axis=1)
-				except:
-					print "single column"
-
-				print 
-
-				print signal.shape
-				# data = signal
-				segmentLimits = rs.silenceRemoval(signal, fs, frame_size, frame_shift)
-				segmentLimits = numpy.asarray(segmentLimits)
-				data = rs.nonsilentRegions(segmentLimits, fs, signal)
-
-				print data.shape
-				
-				print "get lpc"
-				lpc = LPC.extract((fs, data))
-				print "LPC"
-				print type(lpc)
-
-				stfeatures = featureExtraction.stFeatureExtraction(data, fs, window_len, sample_shift )
-				print stfeatures.shape
-
-				# mtfeatures, stfeatures = featureExtraction.mtFeatureExtraction(data, fs, sample_shift*10.0, 2.0*sample_shift, window_len, sample_shift )
-
-				featuresT = stfeatures.transpose()
-				featuresT = numpy.concatenate((featuresT, lpc), axis = 1)
-				print featuresT.shape
+			for fname in utterances:
+				fn = fname.split('/')
+				fn = fn[-1]
+				fno = fno + 1
 				print
+				print fn
+				print fname
+				featuresT = self.getFeaturesFromWave(fname)
+
 				if flag==False:
 					c = featuresT
 					flag = True
@@ -96,37 +49,40 @@ def getFeatureMatrix(direc, num_speakers):
 					y1 = numpy.ones(shape=(featuresT.shape[0],))
 					y1.fill(srno)
 					y = numpy.concatenate((y,y1), axis = 0)
-			except Exception, e:
-				# print fn 
-				raise e
-				print "it produced an error"
 
-	return (c, y)
+		return (c, y)
 
 
+	def getFeaturesFromWave(self, fname):
+		fs, signal = scipy.io.wavfile.read(fname)
+		window_len = self.frame_size*fs 			# Number of samples in frame_size
+		sample_shift = self.frame_shift*fs 		# Number of samples shifted
+		try:
+			if signal.shape[1]:
+				signal = numpy.mean(signal, axis=1)
+		except:
+			print "single column"
+
+		segmentLimits = rs.silenceRemoval(signal, fs, self.frame_size, self.frame_shift)
+		segmentLimits = numpy.asarray(segmentLimits)
+		data = rs.nonsilentRegions(segmentLimits, fs, signal)
+
+		stfeatures = featureExtraction.stFeatureExtraction(data, fs, window_len, sample_shift )
+		lpc = LPC.extract((fs, data))
+		featuresT = stfeatures.transpose()
+		featuresT = numpy.concatenate((featuresT, lpc), axis = 1)
+		return featuresT
 
 
-def getFeaturesFromWave(fname, frame_size = 0.032, frame_shift = 0.016):
-	fs, signal = scipy.io.wavfile.read(fname)
-	window_len = frame_size*fs # Number of samples in frame_size
-	sample_shift = frame_shift*fs # Number of samples shifted
-	try:
-		if signal.shape[1]:
-			signal = numpy.mean(signal, axis=1)
-	except:
-		print "single column"
+	def load_data(self):
+		X, Y = self.getTrainingMatrix()
+		indices  = np.random.permutation(Y.shape[0])
+		X = X[indices, :]
+		Y = Y[indices]
+		train_data_rows = int(Y.shape[0])
 
+		train_x = X[0:train_data_rows+1, :]
+		train_y = Y[0:train_data_rows+1]
+		train_data = (train_x, train_y)
+		return train_data
 
-	segmentLimits = rs.silenceRemoval(signal, fs, frame_size, frame_shift)
-	segmentLimits = numpy.asarray(segmentLimits)
-	data = rs.nonsilentRegions(segmentLimits, fs, signal)
-
-	# data = signal
-	# featuresT = mix_feature((fs, data))
-	stfeatures = featureExtraction.stFeatureExtraction(data, fs, window_len, sample_shift )
-	lpc = LPC.extract((fs, data))
-	featuresT = stfeatures.transpose()
-	featuresT = numpy.concatenate((featuresT, lpc), axis = 1)
-	# mtfeatures, stfeatures = featureExtraction.mtFeatureExtraction(data, fs, sample_shift*10.0, 5.0*sample_shift, window_len, sample_shift )
-	# featuresT = stfeatures.transpose()
-	return featuresT
--- FinalCode/keras1.py
+++ FinalCode/keras1.py
@@ -1,41 +1,30 @@
 import numpy as np
-from sklearn.lda import LDA
+# from sklearn.lda import LDA
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
 import math
 import VQ
-# import applyLDA as al
 import mlpy
 from sklearn.lda import LDA
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
-from keras.regularizers import l2, activity_l2
-from sklearn.preprocessing import OneHotEncoder
-
-
-def load_data(direc, num_speakers):
-	X, Y = VQ.getFeatureMatrix(direc, num_speakers)
-	indices  = np.random.permutation(Y.shape[0])
-
-	X = X[indices, :]
-	Y = Y[indices]
-
-	total_rows = Y.shape[0]
-	train_data_rows = int(total_rows)
-
-	train_x = X[0:train_data_rows+1, :]
-	train_y = Y[0:train_data_rows+1]
-	train_data = (train_x, train_y)
-
-	return train_data
+# from keras.regularizers import l2, activity_l2
+# from sklearn.preprocessing import OneHotEncoder
+from VQ import Features
+# import glob
 
 
 
-direc = "/home/manvi/Desktop/voicebiometric/Phoneme/new_trainset/"
 
 num_speakers = 10
-train_data = load_data(direc, num_speakers)
+direc = "/home/manvi/Desktop/voicebiometric/Phoneme/new_trainset/"
+
+featuresObj = Features(0.032, 0.016, direc, num_speakers)
+
+
+
+train_data = featuresObj.load_data()
 
 X_train = train_data[0]
 y_train = train_data[1]
@@ -48,7 +37,6 @@ print X_train.shape
 # X_train /= std
 # cov = np.dot(X_train.T, X_train) / X_train.shape[0]
 
-
 pca = PCA(n_components='mle', whiten = True) 
 pca.fit(X_train)
 X_train = pca.transform(X_train)
@@ -57,10 +45,10 @@ X_train = pca.transform(X_train)
 print X_train.shape
 print
 print
+
 y_train = np.reshape(y_train, (y_train.shape[0], 1))
 enc = OneHotEncoder()
 y_train =enc.fit_transform(y_train).toarray()
-
 
 
 model = Sequential()
@@ -87,11 +75,8 @@ model.fit(X_train, y_train, nb_epoch=60, validation_split= 0.2)
 
 
 
-
-import glob
-
-direc = "/home/manvi/Desktop/voicebiometric/Phoneme/Test/"
-files = glob.glob(direc + "*.wav")
+testdirec = "/home/manvi/Desktop/voicebiometric/Phoneme/Test/"
+files = glob.glob(testdirec + "*.wav")
 
 
 tot_positives = 0
@@ -103,25 +88,11 @@ while speaker_no<num_speakers:
 	print 
 	print
 	testFile = direc + str(speaker_no) + ".wav"
-	test_data = VQ.getFeaturesFromWave(testFile)  ### frames by features(34)
+	test_data = featuresObj.getFeaturesFromWave(testFile)  ### frames by features(34)
 
 	X_test = test_data
-	# X_test -= mean
-	# X_test /= std
-
-	# print X_test.shape
 	X_test = pca.transform(X_test)
-
-	# X_test = lda.transform(X_test) #using the model to project Z
-	# print X_test.shape
-	# z_labels = lda.predict(Z) #gives you the predicted label for each sample
-	# z_prob = lda.predict_proba(Z)
-
 	modelNN = model.predict(X_test)
-
-	# print modelNN.shape
-	# print type(modelNN)
-
 
 	positives = 0
 	negatives = 0
@@ -135,9 +106,7 @@ while speaker_no<num_speakers:
 
 
 	recall = float(positives*1.0/(modelNN.shape[0]))
-	# prec = float(positives*1.0/(positives+negatives))
 	print "recall = "  + str(recall)
-
 
 	sumRows = []
 	for i in xrange(0,modelNN.shape[1]):
